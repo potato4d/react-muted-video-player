b5d01b9c5b3e99b006fd3c7fa09b5d9134fc815a
Takuma HANATANI <mail@potato4d.me>
2020-03-08 20:20:52 +0900

    chore: fix props bug

--- src/MutedPlayer.tsx
+++ src/MutedPlayer.tsx
@@ -3,6 +3,7 @@ import React, { useState, useRef, useLayoutEffect } from 'react'
 export type MutedPlayerProps = JSX.IntrinsicElements['video']
 
 export const MutedPlayer: React.FC<MutedPlayerProps> = props => {
+  const { autoPlay, muted, playsInline, ...p } = props;
   const [dispatched, setDispatched] = useState(false)
   const videoRef = useRef<HTMLVideoElement>(null)
   useLayoutEffect(() => {
@@ -18,5 +19,10 @@ export const MutedPlayer: React.FC<MutedPlayerProps> = props => {
     videoRef.current.setAttribute('autoplay', '1')
   }, [dispatched, setDispatched])
 
-  return <video {...props} />
+  return <video
+    {...p}
+    autoPlay
+    muted
+    playsInline
+  />
 }
